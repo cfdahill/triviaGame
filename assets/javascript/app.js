47d7cea325680c60401a38005b05e568f6de562e
cfdahill <cfdahill@gmail.com>
2018-03-04 21:38:31 -0700

    Good enough for submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-//objects are the questions and answer:
+//objects are the questions and answer, true means correct:
 var question0 = {
     ask: 'Which of the following is not a minion type?',
     answers: [
@@ -21,66 +21,118 @@ var question2 = {
     ask: 'Which card is known as "4-mana, 7/7?"',
     answers: [
         ["Flamewreathed Faceless", true],
-        ['Faceless Manipulator', false],
+        ["Faceless Manipulator", false],
         ["Faceless Shambler", false],
         ["Faceless Summoner", false]
     ]
 }
+var question3 = {
+    ask: 'What was the name of the first expansion?',
+    answers: [
+        ["The Grand Tournament", false],
+        ["Whispers of the Old Gods", false],
+        ["Journey to Un'Goro", false],
+        ["Goblins vs Gnomes", true]
+    ]
+}
+var question4 = {
+    ask: 'What adventure introduced Sir Finley Mrrgglton?',
+    answers: [
+        ["Blackrock Mountain", false],
+        ["One Night in Karazhan", false],
+        ["Curse of Naxxramas", false],
+        ["League of Explorers", true]
+    ]
+}
+var question5 = {
+    ask: 'What minion has the highest attack value?',
+    answers: [
+        ["The Darkness", false],
+        ["The Ancient One", true],
+        ["Deathwing", false],
+        ["Icehowl", false]
+    ]
+}
 
-var questionArr = [question0, question1, question2];
-var timer = false;
+
+var questionArr = [question0, question1, question2, question3, question4, question5];
 var correctAnswer;
 var userAnswer;
-var z = -1;
+var score = 0;
+var z = 0;
+var time = 14;
+var intervalID;
 
+
+function endRound() {
+    $("#answer").hide();
+    $("button").show();
+    z++;
+    clearInterval(intervalID)
+    time=14;
+    $("#timer").html("Time remaining: 15");
+}
+
+//runGame will make the question and answers visible and determine which answer is correct
 function runGame() {
-    console.log("z before answering =" + z);
-    $("#question").html(questionArr[z].ask);
-    for (i = 0; i < questionArr[z].answers.length; i++) {
-        var short = questionArr[z].answers
-        if (short[i][1] === true) {
-            correctAnswer = short[i][0];
+    if (z < questionArr.length) {
+        $("#question").html(questionArr[z].ask);
+        for (i = 0; i < questionArr[z].answers.length; i++) {
+            var short = questionArr[z].answers
+            if (short[i][1] === true) {
+                correctAnswer = short[i][0];
+            }
+            $("#answer" + i).html(short[i][0]);
         }
-        $("#answer" + i).html(short[i][0]);
-        //The way the code is currently written it is going through the following .click function once on the first question, twice on the 2nd question, 3x on the 3rd question.  Cannot figure out why.  Set it up so it won't really matter except for the score however, need to figure out how to fix this.  Pretty sure it is tied to the forloop and the array position.
+        $(".ansButton").css({"border": "double", "background-color": "rgb(207, 58, 36)"});
+    } else {
+        $("#game").hide();
+        $("#correct").show().html("You got " + score + " out of " + questionArr.length + " correct.");
     }
 }
 
+//countDown makes the timer countdown by 1 second intervals 
+function countDown() {
+    intervalID = setInterval(timer, 1000)     
+}
+
+//timer displays the time remaining, removes a second at each interval, and will stop the timer if the timer reaches 0
+function timer() {
+$("#timer").html("Time remaining: " + time);
+time--;
+if(time < 0) {
+    $("#correct").show().html("Time's up. <br>  The correct answer is: <br>" + correctAnswer);
+    endRound();
+}
+}
+
 $(document).ready(function () {
+    //the following will move onto the next question
     $("button").click(function () {
+        $("#timer").html("Time remaining: " + 15);
         $("button").hide();
         $("button").text("Continue");
         $("#correct").hide();
         $("#answer").show();
-        z++;
-        //console.log(questionArr[z]);
-        runGame()
-
+        runGame();
+        countDown();
     });
+
+    //the following makes all answers clickable and then has two different paths based on if the user picks the right/wrong answer
     $(".ansButton").click(function () {
         userAnswer = $(this).text();
         console.log(userAnswer);
         if (userAnswer === correctAnswer) {
-            $("#answer").hide();
-            $("#correct").show().html("Correct! <br> " + correctAnswer);
-            $("button").show();
-            console.log("z=" + z);
-            userAnswer = "";
+            $("#correct").show().html("<br>Correct! <br> " + correctAnswer);
+            score++;
+            endRound();
         } else {
-            $("#answer").hide();
             $("#correct").show().html("Incorrect. <br>  The correct answer is: <br>" + correctAnswer);
-            $("button").show();
-            console.log("z=" + z);
+            endRound();
         }
     });
 });
 
-
-function countDown() {
-    if (!clockRunning) {
-        intervalID = setInterval(timer, 1000)
-    }
-}
 
 /* Trivia game pseudocode:
 -Make objects with question and 4 possible answers and if it is correct or not
@@ -89,9 +141,10 @@ function countDown() {
 -Get this to be clickable and to confirm correct picks/indicate wrong picks
 -Create a continue button after confirmation
 -Make new question appear by setting objects into an array and calling each individual one through a for loop
+-after the last question, the screen     should show the total number right
 
 -Once the trivia game is working without a timer add a timer component
 -Timer should restart when hitting the start/continue button
 -Timer should stop once an answer is picked
--If timer reaches 0 it should trigger the incorrect answer
+-If timer reaches 0 it should trigger the incorrect answer and stop
 */
